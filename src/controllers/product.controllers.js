d328880329fdf45aafc15ef12f7c7e126ce8d63f
Nicolas Molina <nicolasmolina0920@hotmail.com>
2024-03-12 21:32:49 -0300

    Creando funcion para filtrar los productos por categoria

--- src/controllers/product.controllers.js
+++ src/controllers/product.controllers.js
@@ -1,4 +1,4 @@
-import { productos } from "../db/db.js";
+import { productos } from '../db/db.js';
 
 export const getAllProducts = async (req, res) => {
   // Request == Petición
@@ -8,11 +8,29 @@ export const getAllProducts = async (req, res) => {
 
 export const getProductById = async (req, res) => {
   const { id } = req.params;
-  const productFound = productos.find(product => product.id === id)
- 
+  const productFound = productos.find((product) => product.id === id);
+
   if (productFound) {
-      return res.status(200).json(productFound)
+    return res.status(200).json(productFound);
   }
 
-  res.status(404).json({message: "No hemos podido encontrar el producto solicitado"})
+  res
+    .status(404)
+    .json({ message: 'No hemos podido encontrar el producto solicitado' });
+};
+
+export const getProductByCategory = async (req, res) => {
+  const { categoria } = req.params;
+
+  const filteredProducts = productos.filter((product) => {
+    return product.categoria === categoria;
+  });
+
+  if (filteredProducts.length > 0) {
+    return res.status(200).json(filteredProducts);
+  }
+
+  res.status(404).json({
+    message: `No se encontraron productos en la categoría ${categoria}`,
+  });
 };
