31f4000025d4a94aba41dccc362211515e0be64a
f-iturburu <f.iturburu@outlook.com>
2024-03-13 22:18:53 -0300

    Clase 13/03/2024

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -2,11 +2,13 @@ import { Router } from "express";
 import {
   getAllProducts,
   getProductById,
+  createProduct
 } from "../controllers/product.controllers.js";
 
 const router = Router();
 
 router.get("/productos", getAllProducts);
 router.get("/producto/:id", getProductById);
+router.post("/product", createProduct)
 
 export default router;
