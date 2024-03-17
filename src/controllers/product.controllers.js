b01370a7c4e051ed2705ccd67ffadfb532263a8c
Ezequiel Massa <ezequielmassa.dev@gmail.com>
2024-03-17 17:44:23 -0300

    refactor update product

--- src/controllers/product.controllers.js
+++ src/controllers/product.controllers.js
@@ -77,9 +77,10 @@ export const deleteProduct = async (req, res) => {
 }
 
 export const updateProduct = async (req, res) => {
+	const { id } = req.params
 	try {
-		const { id } = req.params
 		const product = await ProductModel.findById(id)
+		console.log(product)
 		if (!product) {
 			return res.status(404).json({ message: 'Producto no encontrado' })
 		}
@@ -87,6 +88,11 @@ export const updateProduct = async (req, res) => {
 		await product.save()
 		res.status(200).json(product)
 	} catch (error) {
+		if (error.name === 'CastError') {
+			return res
+				.status(404)
+				.json({ message: `No se encontraron productos con el id ${id}` })
+		}
 		res.status(500).json({ message: error.message })
 	}
 }
