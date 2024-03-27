fcf10f7aafc5a7b3d7f3ea6f136b0250eb4e8d6c
FedericoFrau <fedeafrau@gmail.com>
2024-03-27 01:14:16 -0300

    getProductsByCategory and sortProductsByPrice implemented

--- src/controllers/product.controllers.js
+++ src/controllers/product.controllers.js
@@ -1,5 +1,39 @@
 import { products } from "../db/db.js";
 
 export const getAllProducts = async (req,res) => {
-    res.status(200).json(products)
+    res.status(200).json(products);
+};
+
+export const getProductById = async (req,res) => {
+    const { id } = req.params;
+    const productFound = products.find((prod) => prod.id===id);
+    if(productFound) {
+        res.status(200).json(productFound)
+    } else {
+        res.status(404).json({message: "Producto/id no encontrado"})
+    };
+};
+
+export const getProductsByCategory = async (req,res) => {
+    const { category } = req.params;
+    const productsFound = products.filter((prod) => prod.category===category);
+    console.log(productsFound)
+    if(productsFound) {
+        res.status(200).json(productsFound)
+    } else {
+        res.status(404).json({message: "Producto/categoria no encontrado"})
+    }
+};
+
+export const sortProductsByPrice = async(req,res) => {
+    const { sort } = req.params;
+    if(sort==="asc") {
+        const productsSorted = products.sort((a,b) => a.price-b.price)
+        res.status(200).json(productsSorted)
+    } else if(sort==="desc") {
+        const productsSorted = products.sort((a,b) => b.price-a.price)
+        res.status(200).json(productsSorted)
+    } else {
+        res.status(404).json({message: "parámetro ausente o incorrecto, 'sort' debe ser 'asc' o 'desc'"})
+    }
 }
--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -1,8 +1,11 @@
 import { Router } from "express";
-import { getAllProducts } from "../controllers/product.controllers.js";
+import { getAllProducts, getProductById, getProductsByCategory, sortProductsByPrice } from "../controllers/product.controllers.js";
 
 const router = Router();
 
 router.get("/products",getAllProducts)
+router.get("/product/:id",getProductById)
+router.get("/products/category/:category",getProductsByCategory)
+router.get("/products/sort/:sort",sortProductsByPrice)
 
 export default router
